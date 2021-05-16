057d09b7ecc4af15e53d6a22299f605942132408
nakshatra-bazukaa <nakshatravasugupta@gmail.com>
2021-05-16 23:05:54 +0530

    Todo model made

---
+++ models/todo.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema({
+  todoNote: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  deadline: {
+    type: String,
+    required: true,
+  },
+});
+
+const Todo = mongoose.model("Todo", todoSchema);
+
+module.exports = Todo;
